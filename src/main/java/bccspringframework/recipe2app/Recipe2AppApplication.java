5cffad6e2b1cd7f6f39a387f52fcb74426881971
bryan2459 <mvs390@gmail.com>
2021-07-21 10:50:43 +1000

    Initial project Recipe2

---
+++ src/main/java/bccspringframework/recipe2app/Recipe2AppApplication.java
@@ -0,0 +1,13 @@
+package bccspringframework.recipe2app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe2AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe2AppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bccspringframework/recipe2app/Recipe2AppApplicationTests.java
@@ -0,0 +1,13 @@
+package bccspringframework.recipe2app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe2AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
